13680ad56d23e64cb82721e398855a7b84e786c6
Maryan Bagnyuk <marbug@ququdev.com>
2017-03-31 10:28:04 +0300

    fix .sh files

--- scripts/use-bitbucket.sh
+++ scripts/use-bitbucket.sh
@@ -1,5 +1,8 @@
+#!/bin/sh
+
 git remote -v
 
 git remote set-url origin git@bitbucket.org:marbug/igov-vagrant.git
 
 git remote -v
+
--- scripts/use-github.sh
+++ scripts/use-github.sh
@@ -1,5 +1,8 @@
+#!/bin/sh
+
 git remote -v
 
 git remote set-url origin git@github.com:marbug/igov-vagrant.git
 
 git remote -v
+
