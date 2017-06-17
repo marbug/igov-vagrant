b6d37d94dcda3953876133893a6209d56c605eb6
Maryan Bahnyuk <marbuga@gmail.com>
2017-06-17 06:50:12 +0300

    add and use vagrant/scripts/install-node.sh

--- vagrant/install.sh
+++ vagrant/install.sh
@@ -1,3 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 echo "Running " $0 "..."
+
+set -e
+
+vRootDir=`dirname $0`
+echo "Working dir: " $vRootDir
+
+cd $vRootDir
+
+./scripts/install-node.sh
---
+++ vagrant/scripts/install-node.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+vNodeDir="./install/node"
+vTempDir="/tmp/igov"
+
+set -e
+
+echo "Checking presence of" $vNodeDir "..."
+if [ ! -d $vNodeDir ]; then
+  echo $vNodeDir "is absent"
+  exit 1
+fi
+
+echo "Going to" $vNodeDir "..."
+cd $vNodeDir
+
+vArchiveFile=`ls -1 node*.tar.gz | tail -1`
+echo "Node file name:" $vArchiveFile
+
+# TODO: error if no file
+
+echo "Prepareing" $vTempDir "..."
+rm -fR $vTempDir
+mkdir -p $vTempDir
+
+echo "Copying" $vArchiveFile "to" $vTempDir "..."
+cp $vArchiveFile $vTempDir
+
+echo "Going to" $vTempDir
+cd $vTempDir
+
+echo "Extracting" $vArchiveFile "..."
+tar xfz $vArchiveFile
+
+vArchiveDir=`ls -1 | grep -v $vArchiveFile`
+echo "Node dir name:" $vArchiveDir
+
+# TODO: error if no dir
+
+echo "Going to" $vArchiveDir
+cd $vArchiveDir
+
+echo "Archive content:"
+ls -l
+
+echo "Copying bin dir ..."
+sudo cp -r ./bin/* /bin/
+
+echo "Copying lib dir ..."
+sudo cp -r ./lib/* /lib/
+
+echo "Checking version of node ..."
+node -v
+
+# TODO: enhance
